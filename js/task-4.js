21b13326a85af733bc5928e09a928d8f123695d0
Alina <alinapiatakova1309@gmail.com>
2026-04-13 20:52:38 +0300

    initial

---
+++ js/task-4.js
@@ -0,0 +1,56 @@
+const getTotalBalanceByGender=(users, gender)=>{
+let filltered = users.filter((user)=>{return user.gender===gender});
+let total = 0;
+
+  for (const user of filltered) {
+    total += user.balance;
+  }
+
+  return total ;
+}
+
+
+
+
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
